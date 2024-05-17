523be02123df896c33e15afc0dd35540c6f3470d
yoorae <114637091+yrk721@users.noreply.github.com>
2024-05-18 02:07:51 +0900

    fix: useEffect 추가 (#77) (#78)

--- src/pages/luckyBoard/LuckyBoardPage.tsx
+++ src/pages/luckyBoard/LuckyBoardPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { AxiosError, AxiosResponse } from "axios";
 import { ax } from "apis/axios";
 import { LuckyBoardAfterPage, LuckyBoardBeforePage } from ".";
@@ -14,41 +14,45 @@ const LuckyBoardPage: React.FC = () => {
     null
   );
 
-  const fetchLuckyDayData = async () => {
-    try {
-      const res: AxiosResponse<LuckyDayResponse> = await ax.get(
-        "/luckydays/cycl/1",
-        {
-          params: { isCurrent: 1 },
+  useEffect(() => {
+    const fetchLuckyDayData = async () => {
+      try {
+        const res: AxiosResponse<LuckyDayResponse> = await ax.get(
+          "/luckydays/cycl/1",
+          {
+            params: { isCurrent: 1 },
+          }
+        );
+
+        console.log("API 요청 URL:", res.config.url);
+        console.log("API 요청 파라미터:", res.config.params);
+        console.log("API 응답 데이터:", res.data);
+
+        if (res.data && res.data.resData !== null) {
+          setHasCurrentLuckyDay(true);
+        } else {
+          setHasCurrentLuckyDay(false);
         }
-      );
-
-      console.log("API 요청 URL:", res.config.url);
-      console.log("API 요청 파라미터:", res.config.params);
-      console.log("API 응답 데이터:", res.data);
-
-      if (res.data && res.data.resData !== null) {
-        setHasCurrentLuckyDay(true);
-      } else {
-        setHasCurrentLuckyDay(false);
-      }
-    } catch (error) {
-      const axiosError = error as AxiosError<LuckyDayResponse>;
-      console.error("Error fetching lucky day data:", axiosError);
-
-      if (
-        axiosError.response &&
-        axiosError.response.data &&
-        axiosError.response.data.code === "2007"
-      ) {
-        setHasCurrentLuckyDay(false);
-      } else {
+      } catch (error) {
+        const axiosError = error as AxiosError<LuckyDayResponse>;
         console.error("Error fetching lucky day data:", axiosError);
-      }
-    }
-  };
 
-  fetchLuckyDayData();
+        if (
+          axiosError.response &&
+          axiosError.response.data &&
+          axiosError.response.data.code === "2007"
+        ) {
+          setHasCurrentLuckyDay(false);
+        } else {
+          console.error("Error fetching lucky day data:", axiosError);
+        }
+      }
+    };
+
+    fetchLuckyDayData();
+  }, []);
+
+  console.log(hasCurrentLuckyDay);
 
   return (
     <div>
