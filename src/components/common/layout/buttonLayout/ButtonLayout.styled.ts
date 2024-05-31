e95fbf85e5ea133ca7aa4aa2c1ba2883ae523135
zinnli <limgx5192@gmail.com>
2024-05-31 23:09:47 +0900

    레이아웃 스크린 수정

--- src/components/common/layout/buttonLayout/ButtonLayout.styled.ts
+++ src/components/common/layout/buttonLayout/ButtonLayout.styled.ts
@@ -11,6 +11,10 @@ export const ButtonLayout = styled.div`
 
 export const Body = styled.div`
   overflow-y: scroll;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
 `;
 
 export const ButtonWrapper = styled.div`
