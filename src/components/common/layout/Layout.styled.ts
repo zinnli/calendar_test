1a35c59faf0d70eb49bf7b96be26ec9a097ad1b7
zinnli <limgx5192@gmail.com>
2024-05-31 22:35:11 +0900

    레이아웃테스트3

--- src/components/common/layout/Layout.styled.ts
+++ src/components/common/layout/Layout.styled.ts
@@ -4,6 +4,7 @@ import { css } from "@emotion/react";
 export const LayoutContainer = styled.div`
   ${({ theme }) => css`
     display: flex;
+    flex-direction: column;
     align-items: center;
     justify-content: center;
     width: 100%;
