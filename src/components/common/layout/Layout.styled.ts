e5c5e5ee0359c2fd947b3464999a9fa87595d8b8
zinnli <limgx5192@gmail.com>
2024-05-31 22:08:58 +0900

    레이아웃 테스트

--- src/components/common/layout/Layout.styled.ts
+++ src/components/common/layout/Layout.styled.ts
@@ -7,7 +7,7 @@ export const LayoutContainer = styled.div`
     align-items: center;
     justify-content: center;
     width: 100%;
-    height: calc(var(--svh, 1svh) * 100);
+    height: calc(var(--vh, 1vh) * 100 - 80px);
     background-color: ${theme.colors.background};
 
     @supports (-webkit-touch-callout: none) {
